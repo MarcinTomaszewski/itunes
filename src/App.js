15248b1ab336b6942f9cf5749c8ef52d6b1219a6
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2021-06-20 13:16:08 +0200

    Add aplication logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import { Stack } from "@chakra-ui/react"
+import { Button, Input, Stack } from "@chakra-ui/react"
+import { useEffect, useState } from "react";
 import './App.css';
 
+
+
 function App() {
+  const [inputTxt, setInputTxt] = useState("");
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const handleBtnClick = async () => {
+      const result = await fetch(`https:itunes.apple.com/search?term=${encodeURIComponent(inputTxt)}&entity=musicVideo`);
+      const data = await result.json();
+      setResult(data.results);
+    };
+    handleBtnClick();
+  }, [inputTxt])
+
+  const handleInputTxt = e => setInputTxt(e.target.value);
+
+  const handleBtnClick = async () => {
+    const result = await fetch(`https:itunes.apple.com/search?term=${encodeURIComponent(inputTxt)}&entity=musicVideo`);
+    const data = await result.json();
+    setResult(data.results);
+    console.log(data.results);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Stack>
-          Appka
+        <Stack direction={"row"}>
+          <Input
+            value={inputTxt}
+            onChange={handleInputTxt} />
+          <Button
+            colorScheme="blue"
+            onClick={handleBtnClick}>
+            Search
+            </Button>
         </Stack>
       </header>
+      <Stack direction={"column"}>
+        {result.map(item => <p key={item.trackId}>{item.artistName}</p>)}
+      </Stack>
     </div>
   );
 }
